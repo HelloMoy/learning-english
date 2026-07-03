a4e653f25358c85445eef2b686b97f04d5d8d55d
HelloMoy <bruma_moy@hotmail.com>
2026-07-02 21:39:56 -0600

    chore(test-mocks): :wrench: register i18n navigation mock

--- src/test-setup/mocks/index.ts
+++ src/test-setup/mocks/index.ts
@@ -1,3 +1,4 @@
 // Aggregator for global mocks. Importing this file applies all mocks.
 // Add new mocks here as the project grows (e.g. ./auth, ./db, ./analytics).
 import "./next";
+import "./i18n-navigation";
